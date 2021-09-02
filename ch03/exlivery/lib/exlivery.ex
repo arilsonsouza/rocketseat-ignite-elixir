b9032584f878617fb809a96ab45cde2c99439c6b
Arilson Souza <arilsons61@gmail.com>
2021-09-01 21:30:56 -0300

    Making User creation easier

--- ch03/exlivery/lib/exlivery.ex
+++ ch03/exlivery/lib/exlivery.ex
@@ -1,5 +1,10 @@
 defmodule Exlivery do
-  def hello do
-    :world
+  alias Exlivery.Users.CreateOrUpdate
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  def start_agents do
+    UserAgent.start_link(nil)
   end
+
+  defdelegate create_or_update_user(args), to: CreateOrUpdate, as: :call
 end
